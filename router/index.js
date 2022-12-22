be7d8435ea7e35dc0d77e01dfcaf48597158d05d
Deogratias Gemini <dmushi@sovereign-pay.com>
2022-12-22 17:44:33 +0300

    the site is in vue js

--- router/index.js
+++ router/index.js
@@ -9,7 +9,7 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: () => import('../views/Home.vue')
+        component: () => import('../views/Index.vue')
 
     },
     {
--- views/BarChart.vue
+++ views/BarChart.vue
@@ -55,7 +55,7 @@ export default {
         responsive: true
       },
     }
-  }
+  },
   methods: {
     async  getReport(){
       const config = {
