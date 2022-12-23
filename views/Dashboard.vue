6ad7091b577afbfbc5ea7cd4e4545e0d6c14232d
Deogratias Gemini <dmushi@sovereign-pay.com>
2022-12-23 19:18:51 +0300

    addition of logout button

--- views/Dashboard.vue
+++ views/Dashboard.vue
@@ -7,7 +7,7 @@
       <input class="form-control form-control-dark w-100" type="text" placeholder="Tafuta" aria-label="Search">
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="/index.html">Kutoka</a>
+          <router-link to="/login"> Kutoka</router-link>
         </li>
       </ul>
     </nav>
